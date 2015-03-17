6221fa3f0e6b564000ef941f745f75012802b243
Ashok Gelal <ashokgelal@gmail.com>
2015-03-16 21:19:35 -0600

    Ex1: Print all the names in an array using for loop

---
+++ LearnRx.Tests/ArrayTraversing.Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LearnRx.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace LearnRx.Tests
+{
+    [TestClass]
+    public class ArrayTraversing
+    {
+        [TestMethod]
+        public void TestPrintNames()
+        {
+            var logger = Substitute.For<ILogger>();
+            var names = new List<string>();
+            logger.Log(Arg.Do<object>(e=> names.Add(e.ToString())));
+            LearnRx.ArrayTraversing.PrintNames(logger);
+            Assert.AreEqual(8, names.Count);
+            Assert.IsTrue(names.SequenceEqual(new[] {"Ben", "Adam", "Mike", "Billy", "Jacob", "Zach", "Kevin", "Vanessa"}));
+        }
+    }
+}
---
+++ LearnRx/ArrayTraversing.cs
@@ -0,0 +1,16 @@
+﻿using LearnRx.Infrastructure;
+
+namespace LearnRx
+{
+    public class ArrayTraversing
+    {
+        public static void PrintNames(ILogger logger)
+        {
+            var names = new[] {"Ben", "Adam", "Mike", "Billy", "Jacob", "Zach", "Kevin", "Vanessa"};
+            for (var i = 0; i < names.Length; i++)
+            {
+                logger.Log(names[i]);
+           }
+        }
+    }
+}
