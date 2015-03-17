64f4816ea59085309ebdbf1adcec420fd87c5817
Ashok Gelal <ashokgelal@gmail.com>
2015-03-16 23:01:08 -0600

    Ex4: Implement Map()

---
+++ LearnRx/Extensions/CollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearnRx.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static IEnumerable<TE> Map<T, TE>(this IEnumerable<T> coll, Func<T, TE> func)
+        {
+            foreach (var c in coll)
+            {
+                yield return func(c);
+            }
+        }
+    }
+}
